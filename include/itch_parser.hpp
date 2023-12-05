9a0713a1a551920d0a57d13c17039075c7fd9b77
aanrv <varan9595@yahoo.com>
2023-12-05 16:06:03 -0500

    init commit; create itch message structs

---
+++ include/itch_parser.hpp
@@ -0,0 +1,270 @@
+#ifndef ORDER_BOOK_ITCH_PARSER_HPP
+#define ORDER_BOOK_ITCH_PARSER_HPP
+
+#include <cstdint>
+
+namespace ITCH {
+
+struct SystemEventMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     eventCode;
+};
+
+struct StockDirectoryMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint8_t     marketCategory;
+    uint8_t     financialStatusIndicator;
+    uint32_t    roundLotSize;
+    uint8_t     roundLotsOnly;
+    uint8_t     issueClassification;
+    uint8_t     issueSubType[2];
+    uint8_t     authenticity;
+    uint8_t     shortSaleThresholdIndicator;
+    uint8_t     IPOFlag;
+    uint8_t     LULDReferencePriceTier;
+    uint8_t     ETPFlag;
+    uint32_t    ETPLeverageFactor;
+    uint8_t     inverseIndicator;
+};
+
+struct StockTradingActionMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint8_t     tradingState;
+    uint8_t     reserved;
+    uint8_t     reason[4];
+};
+
+struct RegSHORestrictionMessage {
+    uint8_t     messageType;
+    uint16_t    locateCode;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint8_t     RegSHOAction;
+};
+
+struct MarketParticipantPositionMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     MPID[4];
+    uint8_t     stock[8];
+    uint8_t     primaryMarketMaker;
+    uint8_t     marketMakerMode;
+    uint8_t     marketParticipantState;
+};
+
+struct MWCBDeclineLevelMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    level1;
+    uint64_t    level2;
+    uint64_t    level3;
+};
+
+struct MWCBStatusMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     breachedLevel;
+};
+
+struct IPOQuotingPeriodUpdateMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint32_t    IPOQuotationReleaseTime;
+    uint8_t     IPOQuotationReleaseQualifier;
+    uint32_t    IPOPrice;
+};
+
+struct LULDAuctionCollarMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint32_t    auctionCollarReferencePrice;
+    uint32_t    upperAuctionCollarPrice;
+    uint32_t    lowerAuctionCollarPrice;
+    uint32_t    auctionCollarExtension;
+};
+
+struct OperationalHaltMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint8_t     marketCode;
+    uint8_t     operationalHaltAction;
+};
+
+struct AddOrderMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    orderReferenceNumber;
+    uint8_t     buySellIndicator;
+    uint32_t    shares;
+    uint8_t     stock[8];
+    uint32_t    price;
+};
+
+struct AddOrderMPIDAttributionMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    orderReferenceNumber;
+    uint8_t     buySellIndicator;
+    uint32_t    shares;
+    uint8_t     stock[8];
+    uint32_t    price;
+    uint8_t     attribution[4];
+};
+
+struct OrderExecutedMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    orderReferenceNumber;
+    uint32_t    executedShares;
+    uint64_t    matchNumber;
+};
+
+struct OrderExecutedWithPriceMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    orderReferenceNumber;
+    uint32_t    executedShares;
+    uint64_t    matchNumber;
+    uint8_t     printable;
+    uint32_t    executionPrice;
+};
+
+struct OrderCancelMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    orderReferenceNumber;
+    uint32_t    cancelledShares;
+};
+
+struct OrderDeleteMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    orderReferenceNumber;
+};
+
+struct OrderReplaceMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    originalOrderReferenceNumber;
+    uint64_t    newOrderReferenceNumber;
+    uint32_t    shares;
+    uint32_t    price;
+};
+
+struct TradeMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    orderReferenceNumber;
+    uint8_t     buySellIndicator;
+    uint32_t    shares;
+    uint8_t     stock[8];
+    uint32_t    price;
+    uint64_t    matchNumber;
+};
+
+struct CrossTradeMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    shares;
+    uint8_t     stock[8];
+    uint32_t    crossPrice;
+    uint64_t    matchNumber;
+    uint8_t     crossType;
+};
+
+struct BrokenTradeMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    matchNumber;
+};
+
+struct NOIIMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint64_t    pairedShares;
+    uint64_t    imbalanceShares;
+    uint8_t     imbalanceDirection;
+    uint8_t     stock[8];
+    uint32_t    farPrice;
+    uint32_t    nearPrice;
+    uint32_t    currentReferencePrice;
+    uint8_t     crossType;
+    uint8_t     priceVariationIndicator;
+};
+
+struct RetailInterestMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint8_t     InterestFlag;
+};
+
+struct DirectListingWithCapitalRaisePriceDiscoveryMessage {
+    uint8_t     messageType;
+    uint16_t    stockLocate;
+    uint16_t    trackingNumber;
+    uint64_t    timestamp;
+    uint8_t     stock[8];
+    uint8_t     openEligibilityStatus;
+    uint32_t    minimumAllowablePrice;
+    uint32_t    maximumAllowablePrice;
+    uint32_t    nearExecutionPrice;
+    uint64_t    nearExecutionTime;
+    uint32_t    lowerPriceRangeCollar;
+    uint32_t    upperPriceRangeCollar;
+};
+
+} // namespace ITCH
+
+#endif // ORDER_BOOK_ITCH_PARSER_HPP
