cf5019a4340a4765b01ca5252ff769dbc468c05a
aanrv <varan9595@yahoo.com>
2023-12-08 21:52:14 -0500

    add benchmark flag to main.cpp

--- src/itch_reader.cpp
+++ src/itch_reader.cpp
@@ -8,8 +8,7 @@
 
 #include <iostream>                 // todo remove
 
-#define LOG     true
-#define BENCH   true
+#define LOG     false
 #define ASSERT  true
 
 #if ASSERT
@@ -102,77 +101,4 @@ char const * ITCH::Reader::nextMessage() {
 
     return out;
 }
-/*
-void ITCH::Reader::nextMessage() {
-#if BENCH
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::nanoseconds;
-    unsigned long long totalbytes = 0;
-    unsigned long long numMessages = 0;
-    auto t1 = high_resolution_clock::now();
-#endif
 
-    ssize_t bytesRead;
-    size_t offset = 0;
-    bool endOfSession = false;
-    constexpr size_t messageHeaderLength = 2;
-    // read file in chunks of bufferSize bytes (offset handles incomplete message from previous buffer)
-    while (!endOfSession && (bytesRead = read(fdItch, buffer + offset, bufferSize - offset)) > 0) {
-        size_t numValidBytes = bytesRead + offset;                              // number of bytes to process
-                                                                                // handles case where bytesRead < attempted and possibly offset > 0 i.e. memcpy from previous loop
-        offset = 0;
-#if ASSERT
-        assert(numValidBytes <= bufferSize);
-#endif
-
-        char* _buffer = buffer;
-        // read buffer message by message
-        while (_buffer < (buffer + numValidBytes)) {                            // while _buffer pointer has not gone out of bounds for buffer
-            if ((_buffer + messageHeaderLength) > (buffer + numValidBytes)) {
-                // handle case message size is partial
-                // message sizes are 2 bytes but only 1 byte left in buffer
-                offset = 1;
-                buffer[0] = *_buffer;
-                break;
-            }
-            uint16_t messageLength = be16toh(*(uint16_t *)_buffer);             // message header is a 2 byte big endian number containing message length
-            if (messageLength == 0) {
-                // check end of session
-                endOfSession = true;
-                break;
-            }
-            if ((_buffer + messageHeaderLength + messageLength) > (buffer + numValidBytes)) {
-                // handle case current message is partial
-                // message extends past last byte in buffer
-                offset = ((buffer + numValidBytes) - _buffer);  // length of partial message currently in buffer
-                std::memcpy(buffer, _buffer, offset);           // copy length of partial message (offset) from end of _buffer to start of buffer
-                                                                // exit loop, next read destination will start from buffer + offset, and read offset fewer bytes
-                break;
-            }
-            _buffer += messageHeaderLength;
-            //char type = *_buffer;
-            _buffer += (messageLength);
-#if ASSERT
-            assert(_buffer <= (buffer + bufferSize));
-#endif
-
-#if BENCH
-            ++numMessages;
-            totalbytes += (messageHeaderLength + messageLength);
-#endif
-        }
-    }
-#if BENCH
-    auto t2 = high_resolution_clock::now();
-    duration<double, std::nano> ns_double = t2 - t1;
-    std::cout << "processed " << numMessages << " in " << ns_double.count() << " ns" << std::endl;
-    std::cout << "total bytes read " << totalbytes << std::endl;
-#endif
-    if (bytesRead == -1) {
-        std::cerr << "Failed to read bytes from start of file" << std::endl;
-        std::cerr << "Stopping ITCH::Reader::process()" << std::endl;
-    }
-}
-*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include "include/itch_reader.hpp"
+#include <iostream>
+
+#define BENCH true
+
+#if BENCH
+#include <chrono>
+#endif
 
 int main() {
     ITCH::Reader reader("test.txt", 16384);
-    while(reader.nextMessage())
-        ;
+
+#if BENCH
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::nanoseconds;
+    long long messageCount = 0;
+    auto t1 = high_resolution_clock::now();
+#endif
+
+    char const * msg;
+    while((msg = reader.nextMessage())) {
+#if BENCH
+        ++messageCount;
+#endif
+    }
+
+#if BENCH
+    auto t2 = high_resolution_clock::now();
+    duration<double, std::nano> ms_double = t2 - t1;
+    std::cout << "processed " << messageCount << " messages in " << ms_double.count() << " nanoseconds" << std::endl;
+#endif
+
 }
 
